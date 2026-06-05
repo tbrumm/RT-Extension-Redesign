bbfa8fff5170ee21ed185c76b4fc8faede82a673
Torsten Brumm <technik@picturepunxx.de>
2026-06-05 13:14:48 +0200

    Initial project setup with redesigned login page
    - Split-panel login: dark brand area left, form right
    - RT monogram + tagline on brand panel
    - Clean form card without TitleBox decoration
    - Inline CSS, mobile-responsive (brand panel hidden <680px)

---
+++ lib/RT/Extension/Redesign.pm
@@ -0,0 +1,44 @@
+package RT::Extension::Redesign;
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+RT->AddStyleSheets('redesign.css');
+
+1;
+
+=head1 NAME
+
+RT::Extension::Redesign - Modern UI redesign for Request Tracker 6
+
+=head1 DESCRIPTION
+
+A complete UI redesign for RT6. Provides a modern look and feel while
+preserving the standard top navigation structure.
+
+=head1 INSTALLATION
+
+    perl Makefile.PL
+    make
+    sudo make install
+
+Then add to F</opt/rt6/etc/RT_SiteConfig.pm>:
+
+    Plugin('RT::Extension::Redesign');
+
+Clear Mason cache and restart Apache:
+
+    sudo rm -rf /opt/rt6/var/mason_data/obj/*
+    sudo service apache2 restart
+
+=head1 AUTHOR
+
+Torsten Brumm
+
+=head1 LICENSE
+
+GPL version 2
+
+=cut
